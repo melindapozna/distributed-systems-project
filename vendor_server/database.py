e9d40616e37397c20d5e380f3908748f6d2416c3
VasilyLupuleac <v_lupuleac@mail.ru>
2025-04-25 23:53:24 +0300

    Add basic search

--- vendor_server/database.py
+++ vendor_server/database.py
@@ -1,12 +1,25 @@
+from collections.abc import Iterable
 from pymongo import MongoClient
+from re import escape, compile, IGNORECASE
 
 
 class Database:
-    def __init__(self, host, port):
+    def __init__(self, host: str, port: int, name: str):
         self.client = MongoClient(f'mongodb://{host}:{port}')
+        self.db = self.client[name]
 
-    def insert(self, db_name, collection_name, data):
-        self.client[db_name][collection_name].insert_many(data)
+    def insert(self, collection_name: str, data: list[dict]):
+        self.db[collection_name].insert_many(data)
 
-    def select(self, db_name, collection_name):
-        return self.client[db_name][collection_name].find()
+    def select(self, collection_name: str) -> Iterable:
+        return self.db[collection_name].find()
+
+    def search(self, collection_name: str, query: str, fields: list[str]) -> Iterable:
+        db_search_query = {}
+        # Escaping the query string to safely use it in a regular expression
+        search_string = compile(escape(query), flags=IGNORECASE)
+        for field in fields:
+            db_search_query[field] = {'$regex': search_string}
+
+        return self.db[collection_name].find(db_search_query)
+
--- vendor_server/server.py
+++ vendor_server/server.py
@@ -7,16 +7,20 @@ server_port = 1642
 
 db_address = '127.0.0.1'
 db_port = 27017
-db = Database(db_address, db_port)
-db.insert('example', 'values',
-          [{'value': x} for x in [-1000, 10, 1233, 12, -1, 0, 17, 122]])
+db_name = "Products"
+db = Database(db_address, db_port, db_name)
+
+'''book_names = ['Silly Book', 'Medium Book', 'Smart Book']
+book_prices = [10, 30, 50]
+db.insert('Books',
+          [{'name': n, 'price': p} for n, p in zip(book_names, book_prices)])'''
 app = Flask(__name__)
 
 @app.route('/search')
 def search():
     query = request.args.get('query')
-    values = db.select('example', 'values')
-    return [x['value'] for x in values if x['value'] < int(query)]
+    search_results = db.search('Books', query, ['name'])
+    return [x['name'] for x in search_results]
 
 app.run(host=server_address, port=server_port)
 
