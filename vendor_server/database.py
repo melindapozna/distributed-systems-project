197e7ebc85cbb3ba2b528dc77848db17b8046ff1
VasilyLupuleac <v_lupuleac@mail.ru>
2025-04-26 13:53:01 +0300

    Implement purchasing a single item

--- vendor_server/database.py
+++ vendor_server/database.py
@@ -1,4 +1,6 @@
 from collections.abc import Iterable
+
+from bson import ObjectId
 from pymongo import MongoClient
 from re import escape, compile, IGNORECASE
 
@@ -17,9 +19,15 @@ class Database:
     def search(self, collection_name: str, query: str, fields: list[str]) -> Iterable:
         db_search_query = {}
         # Escaping the query string to safely use it in a regular expression
-        search_string = compile(escape(query), flags=IGNORECASE)
+        search_string = compile(escape(query.strip()), flags=IGNORECASE)
         for field in fields:
             db_search_query[field] = {'$regex': search_string}
 
         return self.db[collection_name].find(db_search_query)
 
+    def remove_one(self, collection_name: str, id: str):
+        filter = {'_id': ObjectId(id), 'quantity': {'$gt': 0}}
+        update = {'$inc': {'quantity': -1}}
+        price = self.db[collection_name].find_one(filter)['price']
+        update_result = self.db[collection_name].update_one(filter, update)
+        return update_result.matched_count * price
--- vendor_server/server.py
+++ vendor_server/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from bson import ObjectId
+from flask import Flask, request, jsonify
 
 from vendor_server.database import Database
 
@@ -10,19 +11,36 @@ db_port = 27017
 db_name = "Products"
 db = Database(db_address, db_port, db_name)
 
-'''book_names = ['Silly Book', 'Medium Book', 'Smart Book']
-book_prices = [10, 30, 50]
-db.insert('Books',
-          [{'name': n, 'price': p} for n, p in zip(book_names, book_prices)])'''
+# Switch to False for initial setup
+inserted = True
+if not inserted:
+    books = [('Silly Book', 10, 100), ('Medium Book', 30, 1), ('Smart Book', 50, 0)]
+    db.insert('Books',
+              [{'name': n, 'price': p, 'quantity': q} for n, p, q in books])
+
 app = Flask(__name__)
 
-@app.route('/search')
+
+@app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query')
     search_results = db.search('Books', query, ['name'])
-    return [x['name'] for x in search_results]
+    return jsonify(
+        [{
+            'id': str(x['_id']),
+            'name': x['name'],
+            'price': x['price'],
+            'quantity': x['quantity']
+        } for x in search_results]
+    )
+
+
+@app.route('/buy', methods=['GET'])
+@app.route('/buy', methods=['POST'])
+def buy():
+    body = request.get_json()
+    id = body['id']
+    price = db.remove_one('Books', id)
+    return jsonify(price)
 
 app.run(host=server_address, port=server_port)
-
-
-
