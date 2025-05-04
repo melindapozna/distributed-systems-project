6e6cbe6cf70d4cdb63c9027e5390471c1ddfed35
melindapozna <melindapozna@gmail.com>
2025-05-04 10:37:49 +0300

    Fix configuration

--- vendor_server/server2.py
+++ vendor_server/server2.py
@@ -2,28 +2,23 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv, set_key
 from os import getenv
 
-from vendor_server.database import Database
-from vendor_server.register_server import register_server
 
-# some IDEs may not treat folders as local modules by default, therefore the above 2 imports won't work
-# In that case, comment out the previous 2 imports and uncomment the following 2 lines of code:
-
-# from database import Database
-# from register_server import register_server
+from database import Database
+from register_server import register_server
 
 
 server_address = '127.0.0.1'
 server_port = 1643
 
 # Registering the vendor server with the hub upon first launch
-load_dotenv(dotenv_path='.env.vendor2')
+load_dotenv(dotenv_path='.env2')
 if not getenv('SERVER_REGISTERED'):
     result = register_server(server_address, server_port)
     if not result:
         print('Failed to register the vendor server')
         exit(0)
     print('Successfully registered the vendor server')
-    set_key('.env', 'SERVER_REGISTERED', '1')
+    set_key('.env2', 'SERVER_REGISTERED', '1')
 
 # Database setup
 db_address = '127.0.0.1'
--- vendor_server/setup.py
+++ vendor_server/setup.py
@@ -10,5 +10,5 @@ db.insert('Books', [{'name': n, 'price': p, 'quantity': q} for n, p, q in books]
 
 
 db2.drop_collection('Books')
-books = [('Silly Book 2', 10, 100), ('Medium Book 2', 30, 1), ('Smart Book 2', 50, 0)]
+books = [('Silly Book 2', 10, 20), ('Medium Book 2', 30, 15), ('Smart Book 2', 50, 1    )]
 db2.insert('Books', [{'name': n, 'price': p, 'quantity': q} for n, p, q in books])
